e59839d3e9e85a9bbaa29d7920cc44904bc8dfa4
slashhuang <627284482@qq.com>
2015-11-17 00:01:31 +0800

    coding npm packages

---
+++ node-test/extensions.js
@@ -0,0 +1,12 @@
+/**
+ * Created by slashhuang on 15/11/16.
+ */
+
+console.log(require.extensions['.js'].toString());
+/**
+ * 结果
+ * { '.js': [Function],
+ *    '.json': [Function],
+ *   '.node': [Function: dlopen] }
+ */
+
