b95ddb0bdcc90cfe8807f4c9841f27d03bcd34a4
slashhuang <627284482@qq.com>
2015-11-20 01:39:18 +0800

    finishing webpack and node init

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,15 @@ var webpack = require('webpack');
 module.exports = {
     devtool: 'eval',
     context:process.cwd(),
-    entry:[{app:'./promise/promise'}],
+    entry:{app:'./es6_syntax/promise/promise'},
     output: {
-        path:path.join(__dirname,'dist'),
+        path:path.join(__dirname,'es6_syntax/dist'),
         filename: 'app.js',
-        publicPath: "/static/",//调试地址为相对路径,它会观察所有bundle的文件，从此publicPath更新内存中的js文件获取
+        publicPath: "/static/"//调试地址为相对路径,它会观察所有bundle的文件，从此publicPath更新内存中的js文件获取
     },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()//添加此插件以增加hotmodulereplace功能
+    ],
     module: {
         loaders: [
             {
